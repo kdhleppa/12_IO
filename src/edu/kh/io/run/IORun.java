a9e6a6656e3bfab7252b0aa4e5aa90cd065efbda
kdhleppa <newkdh8624@gmail.com>
2023-08-02 17:42:20 +0900

    0802

---
+++ src/edu/kh/io/run/IORun.java
@@ -0,0 +1,16 @@
+package edu.kh.io.run;
+
+import edu.kh.io.model.service.IOService;
+
+public class IORun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		IOService service = new IOService();
+		
+		// service.output2();
+		service.input2();
+	}
+
+}
